e3054954188c4dcf7c327cab8f42ec885c9b93f9
Theodora2000 <xsimont@stuba.sk>
2021-02-23 10:21:58 +0100

    Array

--- src/main/java/sk/stuba/fei/umi/oop/App.java
+++ src/main/java/sk/stuba/fei/umi/oop/App.java
@@ -1,12 +1,15 @@
 package sk.stuba.fei.umi.oop;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args){
-        String a = "a";
-        String b= "a";
-        String c = Zklavesnice.readString("zadaj text ");
-        System.out.println(a==a);
-        System.out.println(a==b);
-        System.out.println(a==c);
+        int [] x = new int[]{1,2,3};
+        int [] y= new int[3];
+        for(int i=0;i<y.length;i++){
+            y[i]=i+1;
+        }
+        System.out.println(Arrays.equals(x,y));
     }
 }

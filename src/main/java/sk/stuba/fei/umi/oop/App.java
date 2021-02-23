8dfee44cdc73e3a7eeb0970ec2144b067adc2892
Theodora2000 <xsimont@stuba.sk>
2021-02-23 10:13:02 +0100

    Pociatok3

--- src/main/java/sk/stuba/fei/umi/oop/App.java
+++ src/main/java/sk/stuba/fei/umi/oop/App.java
@@ -2,7 +2,11 @@ package sk.stuba.fei.umi.oop;
 
 public class App {
     public static void main(String[] args){
-        int x = Zklavesnice.readInt();
-        System.out.println(x);
+        String a = "a";
+        String b= "a";
+        String c = Zklavesnice.readString("zadaj text ");
+        System.out.println(a==a);
+        System.out.println(a==b);
+        System.out.println(a==c);
     }
 }

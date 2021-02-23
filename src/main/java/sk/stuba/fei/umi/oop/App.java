1323fc8fafb1640b31a1086d6eb22999706c9a3d
Theodora2000 <xsimont@stuba.sk>
2021-02-23 09:56:06 +0100

    Pociatok2

--- src/main/java/sk/stuba/fei/umi/oop/App.java
+++ src/main/java/sk/stuba/fei/umi/oop/App.java
@@ -2,6 +2,7 @@ package sk.stuba.fei.umi.oop;
 
 public class App {
     public static void main(String[] args){
-        System.out.println("mama");
+        int x = Zklavesnice.readInt();
+        System.out.println(x);
     }
 }

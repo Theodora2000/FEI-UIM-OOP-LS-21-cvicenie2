bb01bb2d5e6014ce5f952a459b864d0ae15d6781
Theodora2000 <xsimont@stuba.sk>
2021-02-23 10:40:42 +0100

    nem

--- src/main/java/sk/stuba/fei/umi/oop/App.java
+++ src/main/java/sk/stuba/fei/umi/oop/App.java
@@ -5,11 +5,36 @@ import java.util.Arrays;
 
 public class App {
     public static void main(String[] args){
-        int [] x = new int[]{1,2,3};
-        int [] y= new int[3];
-        for(int i=0;i<y.length;i++){
-            y[i]=i+1;
-        }
-        System.out.println(Arrays.equals(x,y));
+        int x = 5;
+        int [] y = new int[]{1,2,3};
+        FakeStruktura z = new FakeStruktura();
+
+        System.out.println(x);
+        pokusRaw(x);
+        System.out.println(x);
+
+        System.out.println(Arrays.toString(y));
+        pokusArray(y);
+        System.out.println(Arrays.toString(y));
+
+        System.out.println(z.x);
+        z.x=7;
+        System.out.println(z.x);
+        pokusObject(z);
+        System.out.println(z.x);
+    }
+    public static void pokusRaw(int cislo){
+        cislo = 7;
+        System.out.println(cislo);
+    }
+
+    public static void pokusArray(int[] array){
+        array[0]=10;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void pokusObject(FakeStruktura struktura){
+        struktura.x= 10;
+        System.out.println(struktura.x);
     }
 }
---
+++ src/main/java/sk/stuba/fei/umi/oop/FakeStruktura.java
@@ -0,0 +1,6 @@
+package sk.stuba.fei.umi.oop;
+
+public class FakeStruktura {
+    public int x=5;
+
+}
